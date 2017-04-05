2df24f8b32017ee8f76150d163a078123fad0b6c
aatiii <helen_ati@163.com>
2017-04-04 21:34:55 -0400

    edit login register

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,31 @@
 var express = require('express');
 var router = express.Router();
+let passport = require('passport');
+let Account = require('../models/account');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-    title: 'Ads Home'
+    title: 'Ads Home',
+    user: req.user
    });
 });
 
 /* GET register */
 router.get('/register', function(req, res, next) {
   res.render('register', {
-    title: 'Register'
-  });
+    title: 'Register',
+    user: req.user
+});
 });
 
 /* GET login */
 router.get('/login', function(req, res, next) {
 
   res.render('login', {
-    title: 'Login'
+    title: 'Login',
+    user: req.user
+
   });
 });
 
